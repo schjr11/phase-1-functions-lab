49592331b0176c343f6cf9d3f067673af58aa8aa
Scott Hill <scott.c.hill.jr@gmail.com>
2022-01-02 22:53:48 -0600

    commit tree 0264d0cecb1d66ce9d77e65c804a3d954a9fbdf8

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(hqStart){
+    if (hqStart > 42)
+        return hqStart - 42
+    else (hqStart < 42)
+        return 42 - hqStart
+}
+
+function distanceFromHqInFeet(hqStart) {
+    if (hqStart > 42)
+        return (hqStart - 42)*264
+    else (hqStart < 42)
+        return (42 - hqStart)*264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination)
+        return (start - destination)*264
+    else (start < destination)
+        return (destination - start)*264
+}
+
+function calculatesFarePrice(start, destination) {
+    let blockDistance = Math.abs(start - destination)
+    let distanceTravelled = blockDistance * 264;
+    if (distanceTravelled <=400) {
+        console.log("gives customers a free sample");
+        return 0;
+    }
+    else if (distanceTravelled >= 2500) {
+        return "cannot travel that far";
+    }
+    else if (400 < distanceTravelled && distanceTravelled < 2000) {
+        let rideCost = (0.02 * (distanceTravelled - 400));
+        return rideCost;
+    }
+    else if (2000 <=distanceTravelled < 2500) {
+        return 25;
+    }
+}
